5cf70307ac245db87063c37dddfb34759035abff
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-26 11:06:29 +0200

    // Version updated to v1.2.1

--- blockcontactinfos.php
+++ blockcontactinfos.php
@@ -42,7 +42,7 @@ class Blockcontactinfos extends Module
 		$this->name = 'blockcontactinfos';
 		$this->author = 'PrestaShop';
 		$this->tab = 'front_office_features';
-		$this->version = '1.2.0';
+		$this->version = '1.2.1';
 
 		$this->bootstrap = true;
 		parent::__construct();	
